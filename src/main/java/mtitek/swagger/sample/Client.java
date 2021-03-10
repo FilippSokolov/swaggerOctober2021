0aebaf185c1cd9b42f05b89a41dc1d700190238e
Filipp.Sokolov <Filipp.Sokolov@jetbrains.com>
2021-03-10 13:49:48 +0300

    zzz

--- src/main/java/mtitek/swagger/sample/Client.java
+++ src/main/java/mtitek/swagger/sample/Client.java
@@ -12,6 +12,7 @@ public class Client extends Application {
     private final String CONST = "users";
     public void getEndpointTest1() throws MalformedURLException {
         HttpUrl aseUrl1 = HttpUrl.get("http://virtserver.swaggerhub.com/JBtest/Petstorev3Private/1.0.0/pet/");
+        HttpUrl aseUrl2 = HttpUrl.get("mtitek");
 
         System.out.println("sdf");
 
